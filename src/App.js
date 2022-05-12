fe2c6741842e8c35025d3431093bb09c5d639df8
Abdullah <mdabdullahnoman777@gmail.com>
2022-05-12 19:12:55 +0600

    Fornted Complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home/Home";
+import OurProtfolio from "./components/OurProtfolio/OurProtfolio";
+import Contact from "./components/Contact/Contact";
+import Login from "./components/Login/Login/Login";
+import SingUp from "./components/Login/SignUp/SingUp";
+import Navbar from "./components/Shared/Navbar/Navbar";
+import OurTeam from "./components/OurTeam/OurTeam";
+import Footer from "./components/Shared/Footer/Footer";
+import PrivateRoute from "./PrivateRoute";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route
+          path="/protfolio"
+          element={
+            <PrivateRoute>
+              <OurProtfolio></OurProtfolio>
+            </PrivateRoute>
+          }
+        ></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/signup" element={<SingUp></SingUp>}></Route>
+        <Route
+          path="/team"
+          element={
+            <PrivateRoute>
+              <OurTeam></OurTeam>
+            </PrivateRoute>
+          }
+        ></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
-}
+};
 
 export default App;
